c024a615f0602097eeb0160669fc97e94cf44a48
HwngMP <pmai4118@gmail.com>
2026-03-07 23:38:29 +0700

    bubble - operations count

--- source/bubble-sort.cpp
+++ source/bubble-sort.cpp
@@ -21,3 +21,35 @@ void bubble_sort(vector<int> &arr)
         }
     }
 }
+
+void bubble_Sort_OperationCount(vector<int> &nums, int &assignments, int &comparisons)
+{
+    assignments = comparisons = 0;
+
+    int n = nums.size();
+    ++assignments;
+    bool unsorted = true;
+    ++assignments;
+    int idx = 0;
+    ++assignments;
+
+    while (++comparisons, unsorted)
+    {
+        unsorted = false;
+        ++assignments;
+        idx++;
+        ++assignments;
+
+        ++assignments;
+        for (int i = 0; ++comparisons, i < n - idx; i++, ++assignments)
+        {
+            if (++comparisons, nums[i] > nums[i + 1])
+            {
+                swap(nums[i], nums[i + 1]);
+                assignments += 3;
+                unsorted = true;
+                ++assignments;
+            }
+        }
+    }
+}
